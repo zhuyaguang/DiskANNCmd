935c2fefff144c1a0d70ae112d47e948e1bd67bd
zhuyaguang <zhuyaguang1368@163.com>
2022-11-02 10:12:45 +0800

    cmd 加空格

--- README.md
+++ README.md
@@ -16,4 +16,8 @@ cd ..
 ~~~
 ./tests/utils/compute_groundtruth  --data_type float --dist_fn l2 --base_file data/sift/sift_learn.fbin --query_file  data/sift/sift_query.fbin --gt_file data/sift/sift_query_learn_gt100 --K 100
 
+./tests/build_disk_index --data_type float --dist_fn l2 --data_path data/sift/sift_learn.fbin --index_path_prefix data/sift/disk_index_sift_learn_R32_L50_A1.2 -R 32 -L50 -B 0.003 -
+M 1
+ ./tests/search_disk_index  --data_type float --dist_fn l2 --index_path_prefix data/sift/disk_index_sift_learn_R32_L50_A1.2 --query_file data/sift/sift_query.fbin  --gt_file data/sift/sift_query_learn_gt100 -K 10 -L 10 20 30 40 50 100 --result_path data/sift/res --num_nodes_to_cache 10000
+ 
 ~~~
--- main.go
+++ main.go
@@ -87,7 +87,8 @@ func FvecToBin(bin ,fvecPath,fbinPath string)  {
 //ComputeGroundTruth  ./tests/utils/compute_groundtruth  --data_type float --dist_fn l2 --base_file data/sift/sift_learn.fbin --query_file  data/sift/sift_query.fbin --gt_file data/sift/sift_query_learn_gt100 --K 100
 func ComputeGroundTruth(bin ,dataType,distFn,baseFile,queryFile,gtFile ,K string )  {
 	prg := bin+"compute_groundtruth"
-	cmdString := fmt.Sprintf("--data_type "+dataType+"--dist_fn "+distFn+"--base_file "+baseFile+"--query_file "+queryFile+"--gt_file "+gtFile+"--K "+K)
+	cmdString := fmt.Sprintf("--data_type "+dataType+" --dist_fn "+distFn+" --base_file "+baseFile+" --query_file "+queryFile+" --gt_file "+gtFile+" --K  "+K)
+	fmt.Println(cmdString)
 	cmd := exec.Command(prg,cmdString)
 	stdout, err := cmd.Output()
 
@@ -103,7 +104,7 @@ func ComputeGroundTruth(bin ,dataType,distFn,baseFile,queryFile,gtFile ,K string
 func BuildDiskIndex(bin ,dataType,distFn,dataPath ,indexPathPrefix string)  {
 	prg := bin+"build_disk_index"
 	subCmd := " -R 32 -L50 -B 0.003 -M 1"
-	cmdString := fmt.Sprintf("--data_type "+dataType+"--dist_fn "+distFn+"--data_path "+dataPath+"--index_path_prefix "+indexPathPrefix+subCmd)
+	cmdString := fmt.Sprintf("--data_type "+dataType+" --dist_fn "+distFn+" --data_path "+dataPath+" --index_path_prefix "+indexPathPrefix+subCmd)
 	cmd := exec.Command(prg,cmdString)
 	stdout, err := cmd.Output()
 
@@ -118,7 +119,7 @@ func BuildDiskIndex(bin ,dataType,distFn,dataPath ,indexPathPrefix string)  {
 //SearchDiskIndex  ./tests/search_disk_index  --data_type float --dist_fn l2 --index_path_prefix data/sift/disk_index_sift_learn_R32_L50_A1.2 --query_file data/sift/sift_query.fbin  --gt_file data/sift/sift_query_learn_gt100 -K 10 -L 10 20 30 40 50 100 --result_path data/sift/res --num_nodes_to_cache 10000
 func SearchDiskIndex(bin,dataType,distFn,indexPathPrefix,queryFile,gtFile,K ,L,resultPath ,numNodesToCache string)  {
 	prg := bin+"search_disk_index"
-	cmdString := fmt.Sprintf("--data_type "+dataType+"--dist_fn "+distFn+"--index_path_prefix "+indexPathPrefix+"--query_file "+queryFile+"--gt_file "+gtFile+"-K "+K+"-L "+L+"--result_path "+resultPath+"--num_nodes_to_cache "+numNodesToCache)
+	cmdString := fmt.Sprintf("--data_type "+dataType+" --dist_fn "+distFn+" --index_path_prefix "+indexPathPrefix+" --query_file "+queryFile+" --gt_file "+gtFile+" -K "+K+" -L "+L+" --result_path "+resultPath+" --num_nodes_to_cache "+numNodesToCache)
 	cmd := exec.Command(prg,cmdString)
 	stdout, err := cmd.Output()
 
